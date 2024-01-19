47765eddbd450e650e6e65a12e5c5fe7e03c7f77
leemhoon00 <leemhoon000@gmail.com>
2024-01-19 12:44:52 +0900

    build: set path alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from "path";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  resolve: {
+    alias: {
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@src": path.resolve(__dirname, "src"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@routes": path.resolve(__dirname, "src/routes"),
+      "@api": path.resolve(__dirname, "src/api"),
+      "@stores": path.resolve(__dirname, "src/stores"),
+    },
+  },
+});
