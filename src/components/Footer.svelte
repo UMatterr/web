07fe669f1e0a95b4e0c3310dbe20d5759e53e7f4
leemhoon00 <leemhoon000@gmail.com>
2024-01-30 16:22:53 +0900

    design: add setting button at footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -7,6 +7,8 @@
   import homeFillSvg from "@assets/home-fill.svg";
   import personSvg from "@assets/person.svg";
   import personFillSvg from "@assets/person-fill.svg";
+  import settingSvg from "@assets/setting.svg";
+  import settingFillSvg from "@assets/setting-fill.svg";
 
   function homeClick() {
     page = "event";
@@ -15,15 +17,25 @@
   function personClick() {
     page = "friend";
   }
+
+  function settingClick() {
+    page = "setting";
+  }
 </script>
 
 <div>
   {#if page === "event"}
     <img class="item" src={homeFillSvg} alt="" />
     <img on:click={personClick} class="item" src={personSvg} alt="" />
+    <img on:click={settingClick} class="item" src={settingSvg} alt="" />
   {:else if page === "friend"}
     <img on:click={homeClick} class="item" src={homeSvg} alt="" />
     <img class="item" src={personFillSvg} alt="" />
+    <img on:click={settingClick} class="item" src={settingSvg} alt="" />
+  {:else if page === "setting"}
+    <img on:click={homeClick} class="item" src={homeSvg} alt="" />
+    <img on:click={personClick} class="item" src={personSvg} alt="" />
+    <img class="item" src={settingFillSvg} alt="" />
   {/if}
 </div>
 
